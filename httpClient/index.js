923ef4721343c2e83d247a583fb63a817de98abd
Eugenia Miranda <eugemiran@gmail.com>
2022-08-10 16:12:31 -0300

    Set server side page

--- httpClient/index.js
+++ httpClient/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-import applyInterceptors from 'httpClient/interceptors';
+import applyInterceptors from './interceptors';
 
 const APPLICATION_JSON = 'application/json';
 const CONTENT_TYPE = 'Content-Type';
 
 const httpClient = axios.create({
-  baseURL: 'https://rickandmortyapi.com/api',
+  baseURL: 'https://rickandmortyapi.com/api/',
   headers: {
     Accept: APPLICATION_JSON,
     [CONTENT_TYPE]: APPLICATION_JSON,
--- pages/character/[id].tsx
+++ pages/character/[id].tsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from 'react';
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import styles from '../../styles/Home.module.css'
+import CharacterService from '../../services/CharacterService';
 
 const Character: NextPage = () => {
-    return (<h1>First Post</h1>);
+    const router = useRouter();
+    const [data, setData] = useState<any>();
+    const { id } = router.query;
+
+    const getCharacterRequest = async () => {
+        console.log(id)
+        const res = await CharacterService.getCharacter(id);
+        setData(res.data);
+    };
+
+    useEffect(() => {
+       if (id) getCharacterRequest();
+    }, [id]);
+
+    if (!data) return (<p>Loading ...</p>);
+    console.log(data)
+    return (
+    <div>
+        <div>
+            <img src={data.image} /> 
+            <h5 className={styles.names}>{data.name}</h5>
+            <div>{data.species}</div>
+            <div>{data.status}</div>
+        </div>
+    </div>
+    );
 };
 
 export default Character;
